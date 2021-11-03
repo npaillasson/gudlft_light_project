7bc56e331a3f15217956b4c9dbb4e9cd0d6cd410
npaillasson <nathanpaillasson3@gmail.com>
2021-11-03 23:34:20 +0100

    added a test for the disconnection function and added the file test_purchas_places.py for the tests on the reservation functions

--- Test_directory/unit_test/test_book.py
+++ Test_directory/unit_test/test_book.py
@@ -1,12 +1,17 @@
 class TestBooKPage:
-    def test_book_page_return_code_200_with_valid_club_and_competition(self, client):
+    def test_book_page_return_code_200_with_valid_club_and_competition(
+        self,
+        client,
+    ):
         """
         GIVEN a Flask application configured for testing
         WHEN the '/book/<competition>/<club>' page is requested (GET) with a valid competition and a valid club name
         THEN check that the response is valid
         """
 
-        response = client.get("/book/Test competition Festival/Test Club_1")
+        response = client.get(
+            "/book/Test competition Festival/Test Club with enough points to take more than 12 places"
+        )
         assert response.status_code == 200
 
     def test_book_page_return_welcome_template_if_club_is_not_valid(self, client):
@@ -27,7 +32,23 @@ class TestBooKPage:
         WHEN the '/book/<invalid_competition>/<club>' page is requested (GET) with an invalid competition name
         THEN check that the response is valid
         """
-        response = client.get("/book/invalid competition/Test Club 1")
+        response = client.get(
+            "/book/invalid competition/Test Club with enough points to take more than 12 places"
+        )
+        response = response.data.decode()
+        assert response.find("Something went wrong-please try again") != -1
+
+    def test_book_page_is_not_available_if_the_competition_is_outdated(self, client):
+        response = client.get(
+            "/book/Test outdated competition/Test Club with enough points to take more than 12 places"
+        )
+        response = response.data.decode()
+        assert response.find("Something went wrong-please try again") != -1
+
+    def test_book_page_is_not_available_if_the_competition_is_sold_out(self, client):
+        response = client.get(
+            "/book/Test sold out competition/Test Club with enough points to take more than 12 places"
+        )
         response = response.data.decode()
         assert response.find("Something went wrong-please try again") != -1
 
@@ -41,6 +62,8 @@ class TestBooKPage:
         WHEN the '/book/<invalid_competition>/<club>' page is requested (GET) with an invalid competition name
         THEN check that the response is valid
         """
-        response = client.get("/book/Test sold out competition/Test Club 1")
+        response = client.get(
+            "/book/Test sold out competition/Test Club with enough points to take more than 12 places"
+        )
         response = response.data.decode()
         assert response.find("Something went wrong-please try again") != -1
