1fddf5f0a718e5be39ecfd8be365e49cce1643e0
npaillasson <nathanpaillasson3@gmail.com>
2021-11-04 12:08:13 +0100

    added test on the counting of the points of a club and the places of a competition after a successful reservation

--- Test_directory/unit_test/test_purchase_places.py
+++ Test_directory/unit_test/test_purchase_places.py
@@ -1,3 +1,7 @@
+import random
+from server import PRICE_OF_A_REGISTRATION
+
+
 def test_it_is_possible_to_reserve_12_places_in_a_competition_that_is_not_full(
     client, clubs, competitions
 ):
@@ -16,7 +20,9 @@ def test_impossible_to_reserve_more_places_than_available_in_the_competition(
     client, clubs, competitions
 ):
     club = clubs[0]
+    number_of_points_before_booking = int(club["points"])
     competition = competitions[1]
+    number_of_places_before_booking = int(competition["numberOfPlaces"])
     response = client.post(
         "/purchasePlaces",
         follow_redirects=True,
@@ -29,29 +35,36 @@ def test_impossible_to_reserve_more_places_than_available_in_the_competition(
         )
         != -1
     )
+    assert int(club["points"]) == number_of_points_before_booking
+    assert int(competition["numberOfPlaces"]) == number_of_places_before_booking
 
 
 def test_impossible_to_buy_more_places_than_the_club_balance_allows(
     client, clubs, competitions
 ):
     club = clubs[1]
+    number_of_points_before_booking = int(club["points"])
     competition = competitions[0]
+    number_of_places_before_booking = int(competition["numberOfPlaces"])
     response = client.post(
         "/purchasePlaces",
         follow_redirects=True,
         data=dict(club=club["name"], competition=competition["name"], places=12),
     )
     response = response.data.decode()
-    print(response)
     assert (
         response.find("Oops! you don't have enough points to make this reservation...")
         != -1
     )
+    assert int(club["points"]) == number_of_points_before_booking
+    assert int(competition["numberOfPlaces"]) == number_of_places_before_booking
 
 
 def test_impossible_to_buy_more_than_12_places_at_once(client, clubs, competitions):
     club = clubs[0]
+    number_of_points_before_booking = int(club["points"])
     competition = competitions[0]
+    number_of_places_before_booking = int(competition["numberOfPlaces"])
     response = client.post(
         "/purchasePlaces",
         follow_redirects=True,
@@ -64,13 +77,17 @@ def test_impossible_to_buy_more_than_12_places_at_once(client, clubs, competitio
         )
         != -1
     )
+    assert int(club["points"]) == number_of_points_before_booking
+    assert int(competition["numberOfPlaces"]) == number_of_places_before_booking
 
 
 def test_the_page_returns_an_error_if_the_competition_is_full(
     client, clubs, competitions
 ):
     club = clubs[0]
+    number_of_points_before_booking = int(club["points"])
     competition = competitions[2]
+    number_of_places_before_booking = int(competition["numberOfPlaces"])
     response = client.post(
         "/purchasePlaces",
         follow_redirects=True,
@@ -78,13 +95,17 @@ def test_the_page_returns_an_error_if_the_competition_is_full(
     )
     response = response.data.decode()
     assert response.find("Something went wrong-please try again") != -1
+    assert int(club["points"]) == number_of_points_before_booking
+    assert int(competition["numberOfPlaces"]) == number_of_places_before_booking
 
 
-def test_the_page_returns_a_404_error_if_the_competition_is_outdated(
+def test_the_page_returns_an_error_if_the_competition_is_outdated(
     client, clubs, competitions
 ):
     club = clubs[0]
+    number_of_points_before_booking = int(club["points"])
     competition = competitions[3]
+    number_of_places_before_booking = int(competition["numberOfPlaces"])
     response = client.post(
         "/purchasePlaces",
         follow_redirects=True,
@@ -92,15 +113,47 @@ def test_the_page_returns_a_404_error_if_the_competition_is_outdated(
     )
     response = response.data.decode()
     assert response.find("Something went wrong-please try again") != -1
+    assert int(club["points"]) == number_of_points_before_booking
+    assert int(competition["numberOfPlaces"]) == number_of_places_before_booking
 
 
 def test_after_a_successful_reservation_the_number_of_available_places_in_the_competition_is_updated(
-    client,
+    client, clubs, competitions
 ):
-    pass
+    number_of_places_required = random.randrange(1, 13)
+    club = clubs[0]
+    competition = competitions[0]
+    number_of_places_before_booking = int(competition["numberOfPlaces"])
+    places_after_booking = number_of_places_before_booking - number_of_places_required
+    client.post(
+        "/purchasePlaces",
+        follow_redirects=True,
+        data=dict(
+            club=club["name"],
+            competition=competition["name"],
+            places=number_of_places_required,
+        ),
+    )
+    assert int(competition["numberOfPlaces"]) == places_after_booking
 
 
 def test_after_a_successful_reservation_the_number_of_points_of_the_club_is_updated(
-    client,
+    client, clubs, competitions
 ):
-    pass
+    number_of_places_required = random.randrange(1, 13)
+    club = clubs[0]
+    number_of_points_before_booking = int(club["points"])
+    points_after_booking = number_of_points_before_booking - (
+        number_of_places_required * PRICE_OF_A_REGISTRATION
+    )
+    competition = competitions[0]
+    client.post(
+        "/purchasePlaces",
+        follow_redirects=True,
+        data=dict(
+            club=club["name"],
+            competition=competition["name"],
+            places=number_of_places_required,
+        ),
+    )
+    assert int(club["points"]) == points_after_booking
