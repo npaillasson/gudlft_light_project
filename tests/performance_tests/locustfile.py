806c14c67bdf67867718a7c317000e41531325bd
npaillasson <nathanpaillasson3@gmail.com>
2021-11-09 20:33:20 +0100

    update locustfile.py

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -6,11 +6,17 @@ import server
 class ProjectPerfTest(HttpUser):
     @task
     def display_competition_list_and_points_balance(self):
-        self.client.post("/showSummary", {"email": "john@simplylift.co"})
+        with self.client.post(
+            "/showSummary", {"email": "john@simplylift.co"}
+        ) as response:
+            if response.elapsed.total_seconds() > 5:
+                response.failure("Error, the request took too long")
 
     @task
     def purchase_places_and_update_club_pts_and_competition_number_of_places(self):
-        self.client.post(
+        with self.client.post(
             "/purchasePlaces",
             {"competition": "Spring Festival", "club": "Simply Lift", "places": "2"},
-        )
+        ) as response:
+            if response.elapsed.total_seconds() > 2:
+                response.failure("Error, the request took too long")
